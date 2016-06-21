052dea8972ac7e2d46e8ab5e184168247f0c298f
fullstack360 <info@fullstack360.com>
2016-06-20 20:45:11 -0400

    colleges

---
+++ 3-Colleges/Colleges/models/College.swift
@@ -0,0 +1,30 @@
+//
+//  College.swift
+//  Colleges
+//
+//  Created by Dan Kwon on 6/20/16.
+//  Copyright © 2016 Velocity 360. All rights reserved.
+//
+
+import UIKit
+
+class College: NSObject {
+    
+    var name: String!
+    var location: String!
+    var type: String!
+    var mascot: String!
+    var league: String!
+    
+    func populate(collegeInfo: Dictionary<String, AnyObject>){
+        
+        self.name = collegeInfo["name"] as? String
+        self.location = collegeInfo["location"] as? String
+        self.mascot = collegeInfo["mascot"] as? String
+        self.type = collegeInfo["type"] as? String
+        self.league = collegeInfo["league"] as? String
+
+        
+    }
+
+}
